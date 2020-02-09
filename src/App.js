049957962fc3c058a748d0552b0a3e46a0d882e7
SRP2611 <srp261186@gmail.com>
2020-02-09 10:56:27 +0530

    first upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+ const App = () => {
+ 
+ const v1="shalabh"
+ 
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+    <h1> Hello World, {v1}
+      {console.log("Hello how r u")}
+    </h1>
+    
+  );
+}
+
+export default App;
